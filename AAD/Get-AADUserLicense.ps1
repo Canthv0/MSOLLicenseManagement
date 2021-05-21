a42dba821acafadefd08bc4d3b7b463a41d866bd
Matt <matbyrd@forthebyrds.com>
2021-05-21 09:59:14 -0400

    moved location

---
+++ AAD/Get-AADUserLicense.ps1
@@ -0,0 +1,25 @@
+Function Get-AADUserLicense {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $Identity,
+
+        # Parameter help description
+        [Parameter()]
+        [Switch]
+        $All
+    )
+
+    begin{}
+    process{}
+    end{}
+
+
+
+
+
+
+
+
+}
